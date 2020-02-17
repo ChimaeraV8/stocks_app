01020f0050baa9b4f5d6f0252f55fbb41d30f118
ChimaeraV8 <macshirious@icloud.com>
2020-02-17 22:18:56 +0900

    一部haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'stocks#index'
   resources :stocks,only:[:index,:new,:show,:edit]
-  devise_for :users
+  devise_for :users, controllers:{
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   resources :users, only: [:index]
 end
