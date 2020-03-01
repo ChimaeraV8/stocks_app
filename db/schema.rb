bfe73ed7e42b600574ba13361c4ed78c2221f0c6
ChimaeraV8 <macshirious@icloud.com>
2020-03-01 22:02:27 +0900

    テーブル削除
    topicsテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_01_125315) do
+ActiveRecord::Schema.define(version: 2020_03_01_130046) do
 
   create_table "maps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "gs_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
