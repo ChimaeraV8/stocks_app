d24a745b0f0da5c69e027058ef7b95bef1919c8e
ChimaeraV8 <macshirious@icloud.com>
2020-03-17 20:48:10 +0900

    kk

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module StocksApp
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
